fd94d665474d75ba1341375be1c5394655b4b38b
woodong94 <gks3284@naver.com>
2020-01-07 00:54:01 +0900

    code update, for training model

---
+++ ch03/simple_cbow.py
@@ -0,0 +1,45 @@
+import sys
+sys.path.append('..')
+import numpy as np
+from common_wj.layers import MatMul, SoftmaxWithLoss
+
+class simpleCBOW:
+    def __init__(self, vocab_size, hidden_size):
+        V, H = vocab_size, hidden_size  # 인수로 어휘 수와 은닉층의 뉴런 수를 받는다.
+
+        # 가중치 초기화
+        W_in = 0.01 * np.random.randn(V, H).astype('f')  # 32비트 부동소수점 수
+        W_out = 0.01 * np.random.randn(H, V).astype('f')
+
+        # 계층 생성
+        self.in_layer0 = MatMul(W_in)
+        self.in_layer1 = MatMul(W_in)  # W_in은 contexts의 개수만큼 생성 (즉, window_size*2 만큼 생성)
+        self.out_layer = MatMul(W_out)
+        self.loss_layer = SoftmaxWithLoss()
+
+        # 모든 가중치와 기울기를 리스트에 모은다.
+        layers = [self.in_layer0, self.in_layer1, self.out_layer]
+        self.params, self.grads = [], []
+        for layer in layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+        # 인스턴스 변수에 단어의 분산 표현을 저장한다.
+        self.word_vecs = W_in
+
+    def forward(self, contexts, target):  # 인수로 맥락과 타깃을 받아서 loss를 반환
+        # contexts.shape = (6, 2, 7), target.shape = (6, 7)
+        h0 = self.in_layer0.forward(contexts[:, 0])
+        h1 = self.in_layer1.forward(contexts[:, 1])
+        h = (h0 + h1) * 0.5
+        score = self.out_layer.forward(h)
+        loss = self.loss_layer.forward(score, target)
+        return loss
+
+    def backward(self, dout=1):
+        ds = self.loss_layer.backward(dout)
+        da = self.out_layer.backward(ds)
+        da *= 0.5
+        self.in_layer1.backward(da)
+        self.in_layer0.backward(da)
+        return None
--- common_wj/util.py
+++ common_wj/util.py
@@ -112,3 +112,14 @@ def convert_one_hot(corpus, vocab_size):
                 one_hot[idx_0, idx_1, word_id] = 1
 
     return one_hot
+
+def clip_grads(grads, max_norm):
+    total_norm = 0
+    for grad in grads:
+        total_norm += np.sum(grad ** 2)
+    total_norm = np.sqrt(total_norm)
+
+    rate = max_norm / (total_norm + 1e-6)
+    if rate < 1:
+        for grad in grads:
+            grad *= rate
