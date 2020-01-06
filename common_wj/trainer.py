220686f6a3ebffe18bb88a38213df671510c68f8
woodong94 <gks3284@naver.com>
2020-01-07 01:31:47 +0900

    chapter3 마무리

--- common_wj/trainer.py
+++ common_wj/trainer.py
@@ -134,19 +134,6 @@ class RnnlmTrainer:
         plt.ylabel('perplexity')
         plt.show()
 
-
-def clip_grads(grads, max_norm):
-    total_norm = 0
-    for grad in grads:
-        total_norm += np.sum(grad ** 2)
-    total_norm = np.sqrt(total_norm)
-
-    rate = max_norm / (total_norm + 1e-6)
-    if rate < 1:
-        for grad in grads:
-            grad *= rate
-
-
 def remove_duplicate(params, grads):
     '''
     パラメータ配列中の重複する重みをひとつに集約し、
