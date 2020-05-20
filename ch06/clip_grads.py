acc2a82349d72b5cbbb99b487efe2066afce958f
woodongk <gks3284@naver.com>
2020-05-20 11:56:12 +0900

    05-20 ch06-2 ing

---
+++ ch06/clip_grads.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+dW1 = np.random.rand(3,3) * 10
+dW2 = np.random.rand(3,3) * 10
+grads = [dW1, dW2]
+max_norm = 5.0 #threshold
+
+def clip_grads(grads, max_norm):
+    total_norm = 0
+    for grad in grads:
+        total_norm += np.sum(grad ** 2)
+    total_norm = np.sqrt(total_norm)
+        
+    rate = max_norm / (total_norm + 1e-6)
+    if total_norm >= max_norm :
+        for grad in grads:
+            grad *= rate
+            
+display("clipping 전", grads)
+clip_grads(grads,max_norm)
+print()
+display("clipping 후", grads)
--- common/util.py
+++ common/util.py
@@ -149,9 +149,9 @@ def clip_grads(grads, max_norm):
     for grad in grads:
         total_norm += np.sum(grad ** 2)
     total_norm = np.sqrt(total_norm)
-
+        
     rate = max_norm / (total_norm + 1e-6)
-    if rate < 1:
+    if total_norm >= max_norm :
         for grad in grads:
             grad *= rate
 
