6c0b376d06b728fad782e5b984f2b9d2bf1a4b3b
woodongk <gks3284@naver.com>
2020-05-27 23:04:44 +0900

    ch07 진행중

---
+++ ch07_RNN을_사용한_문장_생성/generate_better_text.py
@@ -0,0 +1,39 @@
+import sys
+sys.path.append("..")
+from rnnlm_gen import BetterRnnlmGen
+from dataset import ptb
+
+corpus, word_to_id, id_to_word = ptb.load_data('train')
+vocab_size = len(word_to_id)
+corpus_size = len(corpus)
+
+model = BetterRnnlmGen()
+model.load_params('../ch06_게이트가_추가된_RNN/BetterRnnlm.pkl') # 미리 학습된 가중치 불러와 성능 높임
+
+# 시작 (start) 문자와 건너뛸 (skip) 문자 설정
+start_word = 'you'
+start_id = word_to_id[start_word]
+skip_words = ['N', '<unk>', '$']
+skip_ids = [word_to_id[w] for w in skip_words]
+
+# 문장 생성
+word_ids = model.generate(start_id, skip_ids)
+txt = ' '.join([id_to_word[i] for i in word_ids])
+txt = txt.replace('<eos>', '.\n')
+print(txt)
+
+model.reset_state()  # not continue the previous sequences anymore, now you will start feeding new sequences.
+
+start_words = 'the meaning of life is'
+start_ids = [word_to_id[w] for w in start_words.split(' ')]
+
+for x in start_ids[:-1]:
+    x = np.array(x).reshape(1, 1)
+    model.predict(x)  # shape = (1, 1, 10000)
+
+word_ids = model.generate(start_ids[-1], skip_ids)
+word_ids = start_ids[:-1] + word_ids
+txt = ' '.join([id_to_word[i] for i in word_ids])
+txt = txt.replace(' <eos>', '.\n')
+print('-' * 50)
+print(txt)
