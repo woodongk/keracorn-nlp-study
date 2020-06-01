2eec3f7aeea7748fe5abec293f443b623bfc190a
woodongk <gks3284@naver.com>
2020-06-01 23:29:38 +0900

    파일 정리

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 > 스터디 목적 : 자연어 처리에 관해 심화 학습하면서 밑바닥부터 구현해보기
 
-<img src ="http://www.hanbit.co.kr/data/books/B8950212853_l.jpg" height="250">
+<img src ="imgs/Bookcover.jpg" height="250">
 
 
 ## 목차
--- ch05_순환_신경망(RNN)/simple_rnnlm.py
+++ ch05_순환_신경망(RNN)/simple_rnnlm.py
@@ -3,7 +3,6 @@ sys.path.append("../")
 import numpy as np
 from common.time_layers import *
 
-
 class SimpleRnnlm:
     def __init__(self, vocab_size, wordvec_size, hidden_size):
         V, D, H = vocab_size, wordvec_size, hidden_size
